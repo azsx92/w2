de696ac234bb94c259739dd850a44ee715d2146a
azsx92 <azsx92@gmail.com>
2023-07-14 20:21:52 +0900

    p220 3.3 chap

--- src/main/java/org/zerock/w2/filter/UTF8Filter.java
+++ src/main/java/org/zerock/w2/filter/UTF8Filter.java
@@ -2,12 +2,10 @@ package org.zerock.w2.filter;
 
 import lombok.extern.log4j.Log4j2;
 
-import javax.servlet.annotation.WebFilter;
-
 import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
-import java.util.logging.LogRecord;
 
 @WebFilter(urlPatterns = {"/*"})
 @Log4j2
@@ -15,11 +13,14 @@ public class UTF8Filter implements Filter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        log.info("UTF8 filter...");
-        HttpServletRequest req = (HttpServletRequest) request;
+
+        log.info("UTF8  filter....");
+
+        HttpServletRequest req = (HttpServletRequest)request;
 
         req.setCharacterEncoding("UTF-8");
 
-        chain.doFilter(request,response);
+
+        chain.doFilter(request, response);
     }
 }
